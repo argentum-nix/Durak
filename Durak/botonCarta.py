0691d9dc8ad649eac49237e409675036f5bdf97a
Anastasiia Fedorova <miffyrli@gmail.com>
2020-01-18 01:38:07 -0300

    final

--- Durak/botonCarta.py
+++ Durak/botonCarta.py
@@ -49,6 +49,7 @@ class BotonCarta():
     |   en el caso de que sea su turno de defenderse o atacar,y
     |   además, de que sean cartas posibles de usar y el 
     |   usuario tenga su mouse sobre ella.
+
     '''
 
     def mouseOverButton(self, mode, h):
